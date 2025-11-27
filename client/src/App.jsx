fb85490b579c6c1f14592062d2608c0ddf359a6e
kimu <shuusuke_kaneko@mail.toyota.co.jp>
2025-11-27 10:11:52 +0900

    fix: DropDownコンポーネント化
    Co-authored-by: shuusuke441 <shuusuke441@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,15 @@
 import { useState } from "react";
+import DropDown from "./components/DropDown";
 
 import "./App.css";
 
 function App() {
   const [tag, setTag] = useState("");
 
-  const tagList = [
-    "PC界隈",
-    "服界隈",
-    "釣り界隈",
-    "キャンプ界隈",
-    "筋トレ界隈",
-  ];
-
   return (
     <>
       <h1 className="title">HOBBY✖️HOBBY</h1>
-      <div>
-        タグ選択：
-        <select
-          value={tag}
-          onChange={(e) => {
-            setTag(e.target.value);
-          }}
-          id="pet-select"
-        >
-          {tagList.map((item, i) => (
-            <option value={item} key={i}>
-              {item}
-            </option>
-          ))}
-        </select>
-      </div>
+      <DropDown tag={tag} setTag={setTag} />
     </>
   );
 }
