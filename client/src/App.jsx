d2a4a7357a5b2bd8dd8459f1d1bb9b27d82d02ae
kimu <shuusuke_kaneko@mail.toyota.co.jp>
2025-11-27 09:56:46 +0900

    add: タイトル&ドロップダウン追加
    Co-authored-by: Iwahana-Tou <iwahana-tou@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tag, setTag] = useState("");
+
+  const tagList = [
+    "PC界隈",
+    "服界隈",
+    "釣り界隈",
+    "キャンプ界隈",
+    "筋トレ界隈",
+  ];
 
   return (
     <>
+      <h1 className="title">HOBBY✖️HOBBY</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        タグ選択：
+        <select
+          value={tag}
+          onChange={(e) => {
+            setTag(e.target.value);
+          }}
+          id="pet-select"
+        >
+          {tagList.map((item, i) => (
+            <option value={item} key={i}>
+              {item}
+            </option>
+          ))}
+        </select>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
