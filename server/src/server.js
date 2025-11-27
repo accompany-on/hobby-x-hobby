e30b391ef8a25cbd7270d25c1d6e22408b40cae1
kimu <shuusuke_kaneko@mail.toyota.co.jp>
2025-11-27 23:15:40 +0900

    add:tagsAPIの追加POST,GET

--- server/src/server.js
+++ server/src/server.js
@@ -9,20 +9,49 @@ app.use(express.static("./"));
 app.use(express.json());
 
 app.get("/api/posts", async (req, res) => {
-  const respose = await knex.select().from("posts");
-  res.status(200).json(respose);
+  try {
+    const respose = await knex.select().from("posts");
+    res.status(200).json(respose);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.post("/api/posts", async (req, res) => {
-  const reqBody = req.body;
-  const postData = await knex("posts").insert({
-    title: reqBody.title,
-    comment: reqBody.comment,
-    user_id: reqBody.user_id,
-    link: reqBody.link,
-    tag_id: reqBody.tag_id,
-  });
-  res.status(201).end();
+  try {
+    const reqBody = req.body;
+    const postData = await knex("posts").insert({
+      title: reqBody.title,
+      comment: reqBody.comment,
+      user_id: reqBody.user_id,
+      link: reqBody.link,
+      // tag_id: reqBody.tag_id,
+    });
+    res.status(201).end();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/api/tags", async (req, res) => {
+  try {
+    const tag = await knex.select().from("tags");
+    res.status(200).json(tag);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/api/tags", async (req, res) => {
+  try {
+    const tagBody = req.body;
+    const postData = await knex("tags").insert({
+      name: tagBody.name,
+    });
+    res.status(200).end();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(PORT, () => {
