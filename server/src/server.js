13f8f0b770f8ff8687784538f3d4e35479387630
wataruyagihashi <wataru_yagihashi@mail.toyota.co.jp>
2025-11-28 14:28:07 +0900

    fix: server.jsの静的ファイルのパス修正

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("./"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
