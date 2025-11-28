20808de75fd2890c68bd6a8cce2c9588f0a0240d
Kenta Igarashi <igr4sy@gmail.com>
2025-11-28 10:35:13 +0900

    fix: postsのseedにtag_id追加

--- server/db/seeds/003-tweets.js
+++ server/db/seeds/003-tweets.js
@@ -6,7 +6,17 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('tweets').del();
   await knex('tweets').insert([
-    { user_id: 1, title: 'がらしい', comment: '社長に就任しました！' },
-    { user_id: 2, title: 'トークン', comment: 'トーマスになっちゃった' },
+    {
+      user_id: 1,
+      title: 'がらしい',
+      comment: '社長に就任しました！',
+      tag_id: 1,
+    },
+    {
+      user_id: 2,
+      title: 'トークン',
+      comment: 'トーマスになっちゃった',
+      tag_id: 5,
+    },
   ]);
 };
