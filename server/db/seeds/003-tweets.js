91b67be0c59119d0dc740b31a2cc839ae42b22fb
Kenta Igarashi <igr4sy@gmail.com>
2025-11-28 10:16:13 +0900

    fix: postsテーブル名をtweetsに変更

--- server/db/seeds/003-tweets.js
+++ server/db/seeds/003-tweets.js
@@ -4,8 +4,8 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('posts').del();
-  await knex('posts').insert([
+  await knex('tweets').del();
+  await knex('tweets').insert([
     { user_id: 1, title: 'がらしい', comment: '社長に就任しました！' },
     { user_id: 2, title: 'トークン', comment: 'トーマスになっちゃった' },
   ]);
