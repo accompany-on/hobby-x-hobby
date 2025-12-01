989222d155593226865bc7a6c0e6f67550c6aefa
yuka-matsuki <yuka_nagata@mail.toyota.co.jp>
2025-12-01 14:03:53 +0900

    fix:App.jsx 修正

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -6,7 +6,6 @@ function PostList({ postList }) {
           <div className="post" key={index}>
             <p>タイトル：{post.title}</p>
             <p>テキスト：{post.comment}</p>
-            <p>{console.log(post.tag_id)}</p>
           </div>
         ))}
       </div>
