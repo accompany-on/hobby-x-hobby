b29118586f9dce502a82ef00eee933d13939a053
yuka-matsuki <yuka_nagata@mail.toyota.co.jp>
2025-12-02 14:36:37 +0900

    add:テーブル追加

--- server/db/seeds/002-tags.js
+++ server/db/seeds/002-tags.js
@@ -4,12 +4,19 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('tags').del();
-  await knex('tags').insert([
-    { name: 'PC界隈' },
-    { name: '服界隈' },
-    { name: '釣り界隈' },
-    { name: 'キャンプ界隈' },
-    { name: '筋トレ界隈' },
+  await knex("tags").del();
+  await knex("tags").insert([
+    { name: "PC界隈" },
+    { name: "服界隈" },
+    { name: "釣り界隈" },
+    { name: "キャンプ界隈" },
+    { name: "筋トレ界隈" },
+    { name: "風呂キャン界隈" },
+    { name: "旅行界隈" },
+    { name: "運動好き界隈" },
+    { name: "アニヲタ界隈" },
+    { name: "酒界隈" },
+    { name: "車界隈" },
+    { name: "スポーツ界隈" },
   ]);
 };
